0433be2b1f31dfaa67f3bd95c86abf9cf9e18476
its-shivam1008 <tanil5960@gmail.com>
2025-02-16 00:33:30 +0530

    fix verification email sender

--- backend/middleware/EmailConfig.js
+++ backend/middleware/EmailConfig.js
@@ -1,12 +1,10 @@
 const nodemailer = require("nodemailer");
  const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // true for port 465, false for other ports
+  service: "gmail",
   auth: {
     user: "agarwalvaibhavknp@gmail.com",
     pass: "honk bqho jqrx aatw",
   },
 });
 
-module.exports= transporter;
+module.exports= {transporter};
